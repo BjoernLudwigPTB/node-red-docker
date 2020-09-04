af9ffd465234c141c9550e07a232606c9748a5e5
plkrusePTB <47141905+plkrusePTB@users.noreply.github.com>
2020-09-04 14:12:24 +0200

    wip: tbd: add another image to build in docker-compose

---
+++ latest/twitter/twitter/node-red-contrib-twitter-axios-proxy/twitter-axios-proxy.js
@@ -0,0 +1,14 @@
+module.exports = function(RED) {
+    var Ntwitter = require('twitter-ng');
+   // var axios = require('axios');
+    var crypto = require('crypto');
+    function TwitterCredentialAxiosNode(config){
+        RED.nodes.createNode(this,config);
+        var node =this;
+        node.on('input', function(msg){
+            msg.payload=msg.payload.toLowerCase();
+            node.send(msg);
+        })
+    }
+    RED.nodes.registerType("twitter-axios-proxy",TwitterCredentialAxiosNode);
+}
